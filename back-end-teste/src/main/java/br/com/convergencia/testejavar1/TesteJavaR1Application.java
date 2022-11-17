7e13095bbf386467ced8ebb4d7996fae4457f949
Alex Jose Silva <turmawb@gmail.com>
2022-11-17 07:17:10 -0300

    DEV | Criacao da base do projeto: Back-End e Front-End

---
+++ back-end-teste/src/main/java/br/com/convergencia/testejavar1/TesteJavaR1Application.java
@@ -0,0 +1,13 @@
+package br.com.convergencia.testejavar1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteJavaR1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteJavaR1Application.class, args);
+	}
+
+}
---
+++ back-end-teste/src/test/java/br/com/convergencia/testejavar1/TesteJavaR1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.convergencia.testejavar1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteJavaR1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
